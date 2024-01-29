e273f111df7eb132043e446d3db510bdbad49b02
Jankie <qwq@awaish.link>
2024-01-29 13:44:25 +0800

    Basic AJAX

--- src/include/app.js
+++ src/include/app.js
@@ -4,4 +4,15 @@ class app{
         this.url = url;
         this.id = id;
     }
+
+    download(){
+        var xmlhttp = new XMLHttpRequest();
+        xmlhttp.onreadystatechange = function () {
+            if (request.readyState === 4){
+                if (request.status === 200) {
+                    // Todo: dump request.responseText
+                }
+                else{return false;}
+            }
+    }
 }
