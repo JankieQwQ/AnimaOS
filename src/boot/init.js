32472b5d3ab70a267cdd77f71dae47cac9db3b14
Jankie <qwq@awaish.link>
2024-01-29 13:08:44 +0800

    Booting! File System Check

--- src/boot/init.js
+++ src/boot/init.js
@@ -7,3 +7,21 @@ catch(err) {
     window.close()
 }
 
+console.log('Booting AnimaOS......')
+// 0.Check File System
+const fs = require('fs');
+fs.readdir('.', (err, files) => {
+    if (err) {
+        console.error('Failed to read directory:', err);
+    } else {
+        files.forEach(file => {
+            fs.access(file, fs.constants.R_OK | fs.constants.W_OK, (err) => {
+                if (err) {
+                    console.error(`File ${file} is not readable/writable`);
+                } else {
+                    console.log(`File ${file} is readable/writable`);
+                }
+            });
+        });
+    }
+});
