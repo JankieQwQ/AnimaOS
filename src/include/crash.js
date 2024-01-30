05e8a740da28c6c46dbbc4b40708291bea50b57c
Jankie <qwq@awaish.link>
2024-01-30 12:43:42 +0800

    Dump code to file

--- src/include/crash.js
+++ src/include/crash.js
@@ -1,4 +1,7 @@
 function crash(code=0,message="Unknown error"){
     document.body.style.backgroundColor = "blue";
     document.body.innerHTML = `<h1><code>CRASH</code></h1><br/><p><code>Error Code:${code},${message}</code></p>`;
+    const fs = require('fs');
+    const filename = toString(Math.floor(Date.now() / 1000)) + "_crash.log";
+    fs.writeFile(filename, `${code},${message}`, 'utf8', (err) => {});
 }
