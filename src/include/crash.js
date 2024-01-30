f9fd729f9f6379ffc714c92892a8a00ec6897579
Jankie <qwq@awaish.link>
2024-01-30 12:47:28 +0800

    Show node.js message to user

--- src/include/crash.js
+++ src/include/crash.js
@@ -1,6 +1,6 @@
 function crash(code=0,message="Unknown error"){
     document.body.style.backgroundColor = "blue";
-    document.body.innerHTML = `<h1><code>CRASH</code></h1><br/><p><code>Error Code:${code},${message}</code></p>`;
+    document.body.innerHTML = `<h1><code>CRASH</code></h1><br/><p><code>Error Code:${code},${message},${toString(process.versions)}</code></p>`;
     const fs = require('fs');
     const text = `${code},${message}\n` + toString(process.versions);
     const filename = toString(Math.floor(Date.now() / 1000)) + "_crash.log";
