189aca5c3f276ff6e119456f4eaf75b90153ab02
Jankie <qwq@awaish.link>
2024-01-30 12:44:29 +0800

    Dump runtime message

--- src/include/crash.js
+++ src/include/crash.js
@@ -2,6 +2,7 @@ function crash(code=0,message="Unknown error"){
     document.body.style.backgroundColor = "blue";
     document.body.innerHTML = `<h1><code>CRASH</code></h1><br/><p><code>Error Code:${code},${message}</code></p>`;
     const fs = require('fs');
+    const text = `${code},${message}\n` + toString(process.versions);
     const filename = toString(Math.floor(Date.now() / 1000)) + "_crash.log";
-    fs.writeFile(filename, `${code},${message}`, 'utf8', (err) => {});
+    fs.writeFile(filename, text, 'utf8', (err) => {});
 }
